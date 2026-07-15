a19f021975656724209501f30455fe9b283f6248
Byeong Kwon Kwak <bk.kwak@gmail.com>
2026-07-16 01:54:30 +0900

    feat: assemble full .hwpx package from OwpmlDocument

--- hwp2hwpx/owpml/writer.py
+++ hwp2hwpx/owpml/writer.py
@@ -1,6 +1,9 @@
 """Assemble an OWPML document into a .hwpx ZIP package."""
 import zipfile
 from .. import constants
+from . import package_parts
+from .header_writer import header_xml
+from .section_writer import section_xml
 
 
 def write_package(parts, out_path):
@@ -18,3 +21,18 @@ def write_package(parts, out_path):
             if name == "mimetype":
                 continue
             z.writestr(name, data)
+
+
+def write_hwpx(doc, out_path):
+    parts = {
+        "version.xml": package_parts.version_xml(),
+        "settings.xml": package_parts.settings_xml(),
+        "Contents/header.xml": header_xml(doc.header),
+        "Contents/content.hpf": package_parts.content_hpf(doc.metadata, len(doc.sections)),
+        "META-INF/container.xml": package_parts.container_xml(),
+        "META-INF/manifest.xml": package_parts.manifest_xml(),
+        "Preview/PrvText.txt": package_parts.prv_text(doc.sections),
+    }
+    for i, section in enumerate(doc.sections):
+        parts["Contents/section%d.xml" % i] = section_xml(section)
+    write_package(parts, out_path)
